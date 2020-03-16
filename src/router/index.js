7d7314f5019e494e159dda8583850952cfccc799
unknown <1419528576@qq.com>
2020-03-16 09:01:06 +0800

    路由目录文件的建立

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,14 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = []
+// 按需加载先引入路由组件
+const layout = () => import('@/views/layout')
+const routes = [
+  {
+    path: '/', // 打开默认的就是这个组件
+    component: layout
+  }
+]
 
 const router = new VueRouter({
   routes
---
+++ src/views/layout/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div>我是大哥</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
