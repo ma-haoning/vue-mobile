54c8908b4455f6dbe3dccc6a42c2df000bfeb46a
unknown <1419528576@qq.com>
2020-03-16 08:43:01 +0800

    初始化项目以及适配和引入vant插件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant' // 引入vant模块
+import 'vant/lib/index.css' // 引入vant的css样式
+import 'amfe-flexible' // 引入适配模块的时候需要引入 amfe-flexible
+Vue.use(Vant) // 全局使用vant  任何地方就可以直接使用vant的任何元素
 
 Vue.config.productionTip = false
 
