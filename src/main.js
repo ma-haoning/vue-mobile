676d255294a9d5ca664a4b259ceb503026bbcfc3
unknown <1419528576@qq.com>
2020-03-19 12:33:34 +0800

    登录页面布局和更改vant主题颜色

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant' // 引入vant模块
-import 'vant/lib/index.css' // 引入vant的css样式
+import 'vant/lib/index.less' // 引入vant的css样式  把原来的css样式改成less
 import 'amfe-flexible' // 引入适配模块的时候需要引入 amfe-flexible
 import '@/styles/index.less' // 引入全局样式
 import '@/permission' // 引入路由守卫
