28e78f3b6c381df8e97e8927c3385cd57d0e237e
unknown <1419528576@qq.com>
2020-03-22 21:08:12 +0800

    准备频道编辑组件

--- src/views/layout/home/index.vue
+++ src/views/layout/home/index.vue
@@ -7,25 +7,34 @@
         <articleList :channel_id='item.id'></articleList>
       </van-tab>
     </van-tabs>
-    <div class="bar_btn">
+    <div class="bar_btn" @click="show=true" >
       <!-- 当前的icon在给的css样式中配置好了  直接引入即可 -->
       <van-icon name="wap-nav" />
     </div>
+    <!-- 这个是频道编辑的组件 -->
+    <!-- 当前必须有一个遮罩层 van-action-sheet  绑定v-model就是  让当前的蓶false  只要一点击就把true赋值给v-model  他就会显示啦  -->
+    <!-- round是van-action-sheet的一个属性 默认是true false就是取消圆角  当然必须加上: 如果不加就是字符串 -->
+    <van-action-sheet v-model="show" title="编辑频道" :round="false">
+      <channelsEdit></channelsEdit>
+    </van-action-sheet>
+
   </div>
 </template>
 
 <script>
 import { getChannels } from '@/api/channel' // 引入获取用户频道列表的axios的请求
-import articleList from './components/articleList'
+import articleList from './components/articleList' // 这个是文章列表的组件
+import channelsEdit from './components/channelsEdit'
 export default {
   name: 'home', // devtools查看组件时  可以看到 对应的name名称
   components: {
-    articleList
+    articleList, channelsEdit
   },
   data () {
     return {
       // 设置一个空数组 当请求发送成功之后  把从后台获取到的数据给了当前的channels
-      channels: []
+      channels: [],
+      show: false // 这个是频道编辑组件的  默认显示状态  false是隐藏  当点击对应的图标 true赋值给v-model
     }
   },
   methods: {
@@ -33,7 +42,7 @@ export default {
     async getChannels () {
       // 用异步函数来执行同步代码
       const res = await getChannels()
-      console.log(res)
+      // console.log(res)
       this.channels = res.channels
     }
   },
@@ -103,6 +112,17 @@ export default {
     z-index: 1000;
     &::before {
       font-size: 20px;
+    }
+  }
+}
+.van-action-sheet {
+  max-height: 100%;
+  height: 100%;
+  .van-action-sheet__header {
+    background: #3296fa;
+    color: #fff;
+    .van-icon-close {
+      color: #fff;
     }
   }
 }
